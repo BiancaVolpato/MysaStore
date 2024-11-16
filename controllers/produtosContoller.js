402d981007a3d80379aab9b79fb9020e022b1645
Bianca <93067523+BiancaVolpato@users.noreply.github.com>
2024-11-15 22:05:00 -0300

    Update produtosContoller.js

--- controllers/produtosContoller.js
+++ controllers/produtosContoller.js
@@ -11,7 +11,7 @@ exports.inserirProduto = async (req, res) => {
         .input("descricao", sql.VarChar(255), descricao)
         .input("preco", sql.Decimal(10, 2), preco)
         .input("estoque", sql.Int, estoque)
-        .execute("sp_inserir_produto");
+        .query("INSERT INTO Produtos (nome, descricao, preco, estoque) VALUES (@nome, @descricao, @preco, @estoque)");
         res.status(201).send("Produto inserido com sucesso!");
         } catch (err) {
             res.status(500).send("Erro ao inserir produto: " + err.message);
@@ -26,9 +26,23 @@ exports.atualizarEstoque = async (req, res) => {
         await pool.request()
         .input("produto_id", sql.Int, produto_id)
         .input("quantidade", sql.Int, quantidade)
-        .execute("sp_atualizar_estoque_produto");
+        .query("UPDATE Produtos SET estoque = estoque + @quantidade WHERE produto_id = @produto_id");
         res.send("Estoque atualizado com sucesso!");
         } catch (err) {
             res.status(500).send("Erro ao atualizar estoque: " + err.message);
+        }
+};
+
+// Consultar Produto
+exports.consultarProduto = async (req, res) => {
+    try {
+        const produtoId = req.params.id;
+        const pool = await poolPromise;
+        const result = await pool.request()
+        .input("produto_id", sql.Int, produtoId)
+        .query("SELECT * FROM Produtos WHERE produto_id = @produto_id");
+        res.json(result.recordset[0]);
+        } catch (err) {
+            res.status(500).send("Erro ao consultar produto: " + err.message);
         }
 };
