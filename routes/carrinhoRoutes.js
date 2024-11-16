0110078c1242312a19f7c368ad3e111f42c2f0de
Bianca <93067523+BiancaVolpato@users.noreply.github.com>
2024-11-15 22:05:52 -0300

    Update carrinhoRoutes.js

--- routes/carrinhoRoutes.js
+++ routes/carrinhoRoutes.js
@@ -3,7 +3,8 @@ const express = require("express");
 const router = express.Router();
 const carrinhoController = require("../controllers/carrinhoController");
 
-router.post("/carrinho/adicionar", carrinhoController.adicionarItemCarrinho);
-router.delete("/carrinho/remover/:id", carrinhoController.removerItemCarrinho);
+router.post("/carrinho", carrinhoController.criarCarrinho);
+router.post("/carrinho/item", carrinhoController.adicionarItemCarrinho);
+router.delete("/carrinho/item/:id", carrinhoController.removerItemCarrinho);
 
 module.exports = router;
