339718321a89724c23786729ccc5a895c5fda91e
Bianca <93067523+BiancaVolpato@users.noreply.github.com>
2024-11-15 22:01:53 -0300

    Delete routes/clientesRoutes.js

--- routes/clientesRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/clientesRoutes.js
-const express = require("express");
-const router = express.Router();
-const clientesController = require("../controllers/clientesController");
-
-router.post("/clientes", clientesController.inserirCliente);
-router.get("/clientes/:id", clientesController.consultarCliente);
-
-module.exports = router;
