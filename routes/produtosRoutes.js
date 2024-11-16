44bf2902321ed927192f3600949b2d29aa21800c
Bianca <93067523+BiancaVolpato@users.noreply.github.com>
2024-11-15 22:06:50 -0300

    Update produtosRoutes.js

--- routes/produtosRoutes.js
+++ routes/produtosRoutes.js
@@ -5,5 +5,6 @@ const produtosController = require("../controllers/produtosController");
 
 router.post("/produtos", produtosController.inserirProduto);
 router.put("/produtos/estoque", produtosController.atualizarEstoque);
+router.get("/produtos/:id", produtosController.consultarProduto);
 
 module.exports = router;
